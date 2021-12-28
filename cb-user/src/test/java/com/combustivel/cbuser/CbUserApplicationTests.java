1da8d04fa832a031e6c3fbe76c24850a94fc9d08
Claydson Oliveira <claydsonoliveira@gmail.com>
2021-12-27 21:31:19 -0300

    Created combustivel

---
+++ cb-user/src/test/java/com/combustivel/cbuser/CbUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.combustivel.cbuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
